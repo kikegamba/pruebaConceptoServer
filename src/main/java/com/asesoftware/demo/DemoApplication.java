40ac7625b17f9a412395dea94bb0cd247fc93cb9
kikegamba <we.gamba125@uniandes.edu.co>
2018-09-20 18:20:03 -0500

    Cambio de http en bean

--- src/main/java/com/asesoftware/demo/DemoApplication.java
+++ src/main/java/com/asesoftware/demo/DemoApplication.java
@@ -5,6 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import java.util.stream.Stream;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import java.util.Collections;
+
 
 @SpringBootApplication
 public class DemoApplication {
@@ -26,5 +33,19 @@ public class DemoApplication {
         };
     }
 	
+	@Bean
+@SuppressWarnings("unchecked")
+public FilterRegistrationBean simpleCorsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.setAllowedOrigins(Collections.singletonList("http://prueba-concepto-cliente-6.herokuapp.com"));
+    config.setAllowedMethods(Collections.singletonList("*"));
+    config.setAllowedHeaders(Collections.singletonList("*"));
+    source.registerCorsConfiguration("/**", config);
+    FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+    bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    return bean;
 	
+	}
 }
