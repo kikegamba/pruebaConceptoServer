97d0afde3b9fa73424dd4fb977cb5049ec4e2dd0
kikegamba <we.gamba125@uniandes.edu.co>
2018-09-20 18:01:08 -0500

    Cambio de http

--- src/main/java/com/asesoftware/demo/CarRepository.java
+++ src/main/java/com/asesoftware/demo/CarRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource
-@CrossOrigin(origins = "https://prueba-concepto-cliente-6.herokuapp.com")
+@CrossOrigin(origins = "http://prueba-concepto-cliente-6.herokuapp.com")
 interface CarRepository extends JpaRepository<Car, Long> {
 }
